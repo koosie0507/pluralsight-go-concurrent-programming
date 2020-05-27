380a4559b7580586ca0a431531693868913976fc
Andrei Olar <andrei.olar@complyadvantage.com>
2020-05-27 11:01:10 +0300

    Complete the boilerplate example

--- cmd/app1/main.go
+++ cmd/app1/main.go
@@ -2,12 +2,44 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/koosie0507/pluralsight-go-concurrent-programming/data"
 )
 
+var cache = map[int]data.Book{}
+var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 func main() {
-	for _, book := range data.Books {
-		fmt.Println(book.String())
+	for i:=0; i<10; i++ {
+		id := rnd.Intn(3) + 1
+		if b, ok := queryCache(id); ok {
+			fmt.Println("from cache")
+			fmt.Println(b)
+			continue
+		}
+		if b, ok := queryDb(id); ok {
+			fmt.Println("from db")
+			fmt.Println(b)
+			continue
+		}
+		fmt.Println("book ID ", id, " not found")
+		time.Sleep(150 * time.Millisecond)
 	}
+}
+
+func queryCache(id int) (data.Book, bool) {
+	b, ok := cache[id]
+	return b, ok
+}
+
+func queryDb(id int) (data.Book, bool) {
+	time.Sleep(100 * time.Millisecond)
+	for _, b := range data.Books {
+		if b.ID == id {
+			cache[id] = b
+			return b, true
+		}
+	}
+	return data.Book{}, false
 }
