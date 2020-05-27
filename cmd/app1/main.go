99e34ab768e4882c547362040347cec37e790258
Andrei Olar <andrei.olar@complyadvantage.com>
2020-05-27 11:19:38 +0300

    Add RWMutex to manage access to shared memory

--- cmd/app1/main.go
+++ cmd/app1/main.go
@@ -13,37 +13,42 @@ var cache = map[int]data.Book{}
 var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 func main() {
 	wg := &sync.WaitGroup{}
+	m := &sync.RWMutex{}
 	for i:=0; i<10; i++ {
 		id := rnd.Intn(3) + 1
 		wg.Add(2)
-		go func(id int, wg *sync.WaitGroup) {
-			if b, ok := queryCache(id); ok {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+			if b, ok := queryCache(id, m); ok {
 				fmt.Println("from cache")
 				fmt.Println(b)
 			}
 			wg.Done()
-		}(id, wg)
-		go func(id int, wg *sync.WaitGroup) {
-			if b, ok := queryDb(id); ok {
+		}(id, wg, m)
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+			if b, ok := queryDb(id, m); ok {
 				fmt.Println("from db")
 				fmt.Println(b)
 			}
 			wg.Done()
-		}(id, wg)
+		}(id, wg, m)
 	}
 	wg.Wait()
 }
 
-func queryCache(id int) (data.Book, bool) {
+func queryCache(id int, m *sync.RWMutex) (data.Book, bool) {
+	m.RLock()
 	b, ok := cache[id]
+	m.RUnlock()
 	return b, ok
 }
 
-func queryDb(id int) (data.Book, bool) {
+func queryDb(id int, m *sync.RWMutex) (data.Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range data.Books {
 		if b.ID == id {
+			m.Lock()
 			cache[id] = b
+			m.Unlock()
 			return b, true
 		}
 	}
