3ec34c64fe62ebafc9b6372388a85da63512327f
Andrei Olar <andrei.olar@complyadvantage.com>
2020-05-27 11:03:56 +0300

    Simple goroutines

--- cmd/app1/main.go
+++ cmd/app1/main.go
@@ -13,16 +13,18 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 func main() {
 	for i:=0; i<10; i++ {
 		id := rnd.Intn(3) + 1
-		if b, ok := queryCache(id); ok {
-			fmt.Println("from cache")
-			fmt.Println(b)
-			continue
-		}
-		if b, ok := queryDb(id); ok {
-			fmt.Println("from db")
-			fmt.Println(b)
-			continue
-		}
+		go func(id int) {
+			if b, ok := queryCache(id); ok {
+				fmt.Println("from cache")
+				fmt.Println(b)
+			}
+		}(id)
+		go func(id int) {
+			if b, ok := queryDb(id); ok {
+				fmt.Println("from db")
+				fmt.Println(b)
+			}
+		}(id)
 		fmt.Println("book ID ", id, " not found")
 		time.Sleep(150 * time.Millisecond)
 	}
