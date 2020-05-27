4c20bacab13c39f250eeea53277c52021e43ee4c
Andrei Olar <andrei.olar@complyadvantage.com>
2020-05-27 12:25:21 +0300

    Add channels to coordinate db and cache access

--- cmd/app1/main.go
+++ cmd/app1/main.go
@@ -14,23 +14,39 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 func main() {
 	wg := &sync.WaitGroup{}
 	m := &sync.RWMutex{}
-	for i:=0; i<10; i++ {
+	cacheChan := make(chan data.Book)
+	dbChan := make(chan data.Book)
+	for i:=0; i<3; i++ {
 		id := rnd.Intn(3) + 1
-		wg.Add(2)
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		wg.Add(3)
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- data.Book) {
 			if b, ok := queryCache(id, m); ok {
-				fmt.Println("from cache")
-				fmt.Println(b)
+				ch <- b
 			}
 			wg.Done()
-		}(id, wg, m)
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		}(id, wg, m, cacheChan)
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan<- data.Book) {
 			if b, ok := queryDb(id, m); ok {
-				fmt.Println("from db")
-				fmt.Println(b)
+				m.Lock()
+				cache[id] = b
+				m.Unlock()
+				ch <- b
 			}
 			wg.Done()
-		}(id, wg, m)
+		}(id, wg, m, dbChan)
+		go func(cacheCh, dbCh <-chan data.Book, wg *sync.WaitGroup) {
+			select{
+			case cachedBook := <-cacheCh:
+				fmt.Println("from cache")
+				fmt.Println(cachedBook)
+				<-dbCh
+			case databaseBook := <-dbCh:
+				fmt.Println("from db")
+				fmt.Println(databaseBook)
+			}
+			wg.Done()
+		}(cacheChan, dbChan, wg)
+		time.Sleep(100 * time.Millisecond)
 	}
 	wg.Wait()
 }
@@ -46,9 +62,6 @@ func queryDb(id int, m *sync.RWMutex) (data.Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range data.Books {
 		if b.ID == id {
-			m.Lock()
-			cache[id] = b
-			m.Unlock()
 			return b, true
 		}
 	}
